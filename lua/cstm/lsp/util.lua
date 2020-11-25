2d794190f12f9bba96577df03ffb630814294dda
Aliou Diallo <code@aliou.me>
2020-11-25 09:13:17 +0100

    Improve check of running LSP clients for the buffer.

--- lua/cstm/lsp/util.lua
+++ lua/cstm/lsp/util.lua
@@ -2,9 +2,12 @@ local lsp = require('vim.lsp')
 
 local M = {}
 
+-- Check if LSP is running by checking if we have clients for the current
+-- buffer.
+-- This will probably will be deleted once everything related to LSP is moved to
+-- Lua files.
 M.is_running = function()
-  -- TODO: Check active clients for the current buffer.
-  return not vim.tbl_isempty(lsp.get_active_clients())
+  return #lsp.buf_get_clients() > 0
 end
 
 return M
