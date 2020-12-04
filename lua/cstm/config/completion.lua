2cc094e221c835cf5352a009b8474e06976e42d0
Aliou Diallo <code@aliou.me>
2020-12-04 16:57:56 +0100

    Improve completion configuration.

--- lua/cstm/config/completion.lua
+++ lua/cstm/config/completion.lua
@@ -2,15 +2,21 @@
 vim.g.completion_trigger_keyword_length = 2
 
 -- Trigger when deleting stuff.
-vim.g.completion_trigger_on_delete = 1
+vim.g.completion_trigger_on_delete = true
+
+-- Automatically change completion source when there's no completion item.
+vim.g.completion_auto_change_source = true
 
 -- Configure completion chaining.
 vim.g.completion_chain_complete_list = {
   default = {
-    { complete_items = {'lsp', 'tags'} },
-    { complete_items = {'path'}, triggered_only = {'/'} },
-    { mode = '<c-p>' },
-    { mode = '<c-n>' },
-  },
-  comment = {}
+    default = {
+      { complete_items = {'lsp'} },
+      { complete_items = {'tags'} },
+      { complete_items = {'path'}, triggered_only = {'/'} },
+      { mode = '<c-p>' },
+      { mode = '<c-n>' },
+    },
+    comment = {}
+  }
 }
