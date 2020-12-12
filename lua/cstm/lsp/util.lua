83d78561717329f7f1b80c8cf2123d1ee6fd5719
Aliou Diallo <code@aliou.me>
2020-12-12 23:10:22 +0100

    Remove unused file.

--- lua/cstm/lsp/util.lua
+++ lua/cstm/lsp/util.lua
@@ -1,17 +1,7 @@
-local lsp = require('vim.lsp')
-
-local M = {}
-
--- Check if LSP is running by checking if we have clients for the current
--- buffer.
--- This will probably will be deleted once everything related to LSP is moved to
--- Lua files.
-M.is_running = function()
-  return #lsp.buf_get_clients() > 0
-end
-
-M.map = function(mode, key, result)
+local map = function(mode, key, result)
   vim.api.nvim_buf_set_keymap(0, mode, key, result, { noremap = true, silent = true })
 end
 
-return M
+return {
+  map = map
+}
