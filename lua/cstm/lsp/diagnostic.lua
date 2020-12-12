f5fdff160b10d7f54275cfff4a5753713e51916d
Aliou Diallo <code@aliou.me>
2020-12-12 23:14:13 +0100

    Use ALE maps for LSP functions.

--- lua/cstm/lsp/diagnostic.lua
+++ lua/cstm/lsp/diagnostic.lua
@@ -9,17 +9,12 @@ local on_attach = function(_)
   vim.b.lsp_diagnostics_update_in_insert_enabled = true
 
   -- Navigate around warnings / errors.
-  -- TODO: At some point merge these maps with the error maps from ALE, see
-  -- config/plugin/ale/maps.vim
-  map("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev({ wrap = true })<cr>")
-  map("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next({ wrap = true })<cr>")
-  map("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next({ wrap = true })<cr>")
+  map("n", "[a", "<cmd>silent lua vim.lsp.diagnostic.goto_prev({ wrap = true })<cr>")
+  map("n", "]a", "<cmd>silent lua vim.lsp.diagnostic.goto_next({ wrap = true })<cr>")
 
 
   -- Toggle diagnostics for current buffer.
-  -- TODO: At some point, also merge this map with the buffer toggle from ALE,
-  -- see config/plugin/ale/maps.vim
-  map("n", "<leader>dt", "<cmd>lua require('cstm.buffer.diagnostic').toggle()<cr>")
+  map("n", "<leader>at", "<cmd>lua require('cstm.buffer.diagnostic').toggle()<cr>")
 end
 
 return {
--- lua/cstm/lsp/rename.lua
+++ lua/cstm/lsp/rename.lua
@@ -3,7 +3,9 @@ local map = require('cstm.lsp.util').map
 local on_attach = function(client)
   if not client.resolved_capabilities.rename then return end
 
+  -- TODO: Remove the less used mapping.
   map("n", "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<CR>")
+  map("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
 end
 
 return {
