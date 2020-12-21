7c6e985e919026b57f8ad8358ead91f2757843b6
Aliou Diallo <code@aliou.me>
2020-12-21 12:15:53 +0100

    Simplify runtimepath setup.

--- init.lua
+++ init.lua
@@ -1,24 +1,24 @@
 local api = vim.api
 
+-- Setup runtimepath and packpath to include ~/.vim directory..
 -- TODO: Use tjdevries's vim.opt when / if it is merged into core.
 local runtimepath = api.nvim_get_option('runtimepath')
 
--- Set the vim directory as the first directory to search into.
-runtimepath = '~/.vim,' .. runtimepath
+-- Set the vim directory as the first directory to search into and the after
+-- directory as the last to search into.
+runtimepath = '~/.vim,' .. runtimepath .. ',~/.vim/after'
 
--- Add the after directory in the directory to search into.
-runtimepath = runtimepath .. ',~/.vim/after'
-
--- Save the option.
+-- Save the option and update the packpath value to match.
 api.nvim_set_option('runtimepath', runtimepath)
-
--- Update the directories to find packages to include our updates.
 api.nvim_set_option('packpath', runtimepath)
 
 -- Setting up early so mappings with <leader> use the this value.
 vim.g.mapleader = ','
 
--- Require packages.
+-- Require files to bootstrap configuration.
+require('vim.registers')
+
+-- Require plugins.
 require('cstm.packages')
 
 -- Load split configuration files.
