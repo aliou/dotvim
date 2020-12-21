8648075e74ea21ac569c358bf4f6783043619fa7
Aliou Diallo <code@aliou.me>
2020-12-21 12:13:21 +0100

    Add lua interfact to vim registers.

---
+++ lua/vim/registers.lua
@@ -0,0 +1,13 @@
+-- TODO: Use the table in the __index and __newindex so we can inspect the
+-- vim.registers variable.
+local register_mt = {
+  __index = function(_, register_name)
+    return vim.fn.getreg(register_name)
+  end,
+
+  __newindex = function(_, register_name, value)
+    vim.fn.setreg(register_name, value)
+  end,
+}
+
+vim.registers = setmetatable({}, register_mt)
