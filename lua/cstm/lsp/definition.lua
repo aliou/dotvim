418609b84e02d3b43e1fd6e869781d1c77ae519c
Aliou Diallo <code@aliou.me>
2020-12-27 12:07:16 +0100

    Use ctags to jump to definition for ruby.

--- lua/cstm/lsp/definition.lua
+++ lua/cstm/lsp/definition.lua
@@ -5,6 +5,11 @@ local on_attach = function(client)
     return
   end
 
+  -- Fallback to the default behaviour (ctags) for definition.
+  if vim.api.nvim_buf_get_option(0, 'filetype') == 'ruby' then
+    return
+  end
+
   map("n", "<C-]>", "<cmd>lua vim.lsp.buf.definition()<CR>")
 end
 
