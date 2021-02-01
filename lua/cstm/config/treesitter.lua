506602071ad259698633e6d049deb5a611400208
Aliou Diallo <code@aliou.me>
2021-02-01 12:17:48 +0100

    Only use Treesitter for go.
    Ruby doesn't seem to work as expected.

--- lua/cstm/config/treesitter.lua
+++ lua/cstm/config/treesitter.lua
@@ -1,21 +1,6 @@
 require('nvim-treesitter.configs').setup({
-  ensure_installed = { "ruby", "rust", "lua" },
-  -- TODO: Add custom highlights for rspec focus and skipped things.
+  ensure_installed = { "go" },
   highlight = {
-    enable = true,
-  },
-  indent = {
-    enable = true,
-    disable = { "lua" }
+    enable = { "go" },
   },
 })
-
--- Fix issue with nvim-treesitter and Ruby where `%` and highlighting pairs
--- don't correctly work.
-vim.api.nvim_exec([[
-  augroup nvim.treesitter
-    autocmd!
-
-    autocmd BufNewFile,BufRead *.rb set syntax=ruby
-  augroup END
-]], true)
