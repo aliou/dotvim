3e5ee8e7ddc9175cf019590d52706e5a967a833b
Aliou Diallo <code@aliou.me>
2021-02-04 19:31:33 +0100

    Display in status line progress of lsp.buf.references.

--- autoload/status/lsp.vim
+++ autoload/status/lsp.vim
@@ -2,8 +2,11 @@ let s:in_progress = '⋯'
 let s:failure = '✗'
 let s:success = '✓'
 
-" TODO: In some cases this seems to silently fail? (maybe when there's no lsp client running?)
 function! status#lsp#diagnostic() abort
+  if exists('b:status_value')
+    return b:status_value
+  end
+
   if luaeval('not vim.tbl_isempty(vim.lsp.diagnostic.get(' . bufnr() . '))')
     return s:failure
   endif
--- lua/cstm/status/init.lua
+++ lua/cstm/status/init.lua
@@ -1,6 +1,20 @@
 -- Delegate to lightline for now.
 local refresh = vim.fn['lightline#update']
 
+local in_progress = '⋯'
+local failure = '✗'
+local success = '✓'
+
+local set_status = function(status)
+  vim.b.status_value = status
+  refresh()
+end
+
 return {
-  refresh = refresh
+  refresh = refresh,
+  set_status = set_status,
+
+  in_progress = in_progress,
+  failure = failure,
+  success = success,
 }
