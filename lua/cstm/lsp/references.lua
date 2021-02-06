b8f609191dce2e4e9a3e7b04493ae08638f1c396
Aliou Diallo <code@aliou.me>
2021-02-06 14:21:23 +0100

    Update reference mapping.

--- lua/cstm/lsp/references.lua
+++ lua/cstm/lsp/references.lua
@@ -1,7 +1,7 @@
 local map = require('cstm.util').map
 
 local on_attach = function(_)
-  map("n", "<C-[>", "<cmd>lua vim.lsp.buf.references()<CR>")
+  map("n", "<C-\\>", "<cmd>lua vim.lsp.buf.references()<CR>")
 end
 
 return {
