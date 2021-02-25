b505acfbb8cbaaeb97d6418d7e9baa3a15a88d5c
Aliou Diallo <code@aliou.me>
2021-02-25 08:49:57 +0100

    Use rubocop-daemon-wrapper with efm.

--- lua/cstm/lsp/server/efm/rubocop.lua
+++ lua/cstm/lsp/server/efm/rubocop.lua
@@ -3,4 +3,12 @@ return {
   lintStdin = true,
   lintFormats = {"%f:%l:%c: %t: %m"},
   lintIgnoreExitCode = true,
+  -- Rubocop fixer outputs diagnostics first and then the fixed
+  -- output. These are delimited by a "=======" string that we
+  -- look for to remove everything before it.
+  --
+  -- The awk program finds the pattern, sets p to 1 and then skip the remaininig
+  -- pattern on the line. It then prints what remains.
+  formatCommand = "rubocop-daemon-wrapper --auto-correct --stdin ${INPUT} | awk '/====================/{p=1;next}p'",
+  formatStdin = true
 }
