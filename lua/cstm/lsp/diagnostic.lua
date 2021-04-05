b60336b5303d27b7b397e4bab1b5fd7c6bc36dc0
Aliou Diallo <code@aliou.me>
2021-04-05 18:32:36 +0200

    Reapply highlight overrides on colorscheme change.

--- lua/cstm/lsp/diagnostic.lua
+++ lua/cstm/lsp/diagnostic.lua
@@ -5,10 +5,19 @@ vim.g.lsp_diagnostics_enabled = true
 vim.g.lsp_diagnostics_update_in_insert_enabled = true
 
 -- Configure virtual text colors.
-vim.highlight.create('LspDiagnosticsVirtualTextError', { cterm = 'bold', ctermfg = 167, gui = 'bold', guifg = '#CC6666' })
-vim.highlight.create('LspDiagnosticsVirtualTextWarning', { ctermfg = 173, gui = 'bold', guifg = '#de935f' })
-vim.highlight.create('LspDiagnosticsVirtualTextInformation', { cterm = 'bold', ctermfg = 60, gui = 'bold', guifg = '#5F5F87' })
-vim.highlight.create('LspDiagnosticsVirtualTextHint', { cterm = 'bold', ctermfg = 173, gui = 'bold', guifg = '#c7915b' })
+local apply_highlights = function()
+  vim.highlight.create('LspDiagnosticsVirtualTextError', { cterm = 'bold', ctermfg = 167, gui = 'bold', guifg = '#CC6666' })
+  vim.highlight.create('LspDiagnosticsVirtualTextWarning', { ctermfg = 173, gui = 'bold', guifg = '#de935f' })
+  vim.highlight.create('LspDiagnosticsVirtualTextInformation', { cterm = 'bold', ctermfg = 60, gui = 'bold', guifg = '#5F5F87' })
+  vim.highlight.create('LspDiagnosticsVirtualTextHint', { cterm = 'bold', ctermfg = 173, gui = 'bold', guifg = '#c7915b' })
+end
+
+vim.cmd [[augroup lsp.diagnostic_highlights]]
+vim.cmd [[  autocmd!]]
+vim.cmd [[  autocmd ColorScheme :lua require('cstm.lsp.diagnostic').apply_highlights()]]
+vim.cmd [[augroup END]]
+
+apply_highlights()
 
 local on_attach = function(_)
   -- Setup local diagnostics toggles.
@@ -24,5 +33,6 @@ local on_attach = function(_)
 end
 
 return {
-  on_attach = on_attach
+  on_attach = on_attach,
+  apply_highlights = apply_highlights
 }
