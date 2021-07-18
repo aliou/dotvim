1c30ea0ccb33022821a93f7d7aa0c3d54675b6a2
Aliou Diallo <code@aliou.me>
2021-07-18 21:51:52 +0200

    Revert back to vimscript version of visual_search.

---
+++ lua/ad/lightline.lua
@@ -0,0 +1,10 @@
+local on_theme_change = require('ad.theme').on_theme_change
+
+-- TODO: Change this with custom cool themes :)
+DEFAULT_CONFIG = { dark = 'default', light = 'one' }
+
+on_theme_change(function(theme)
+  local lightline_theme = DEFAULT_CONFIG[theme]
+  vim.g.lightline.colorscheme = lightline_theme
+  vim.fn['lightline#update']()
+end)
