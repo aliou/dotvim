c53671d75db401218007cf3c38ee3ea7bc326153
Aliou Diallo <code@aliou.me>
2021-07-18 21:50:48 +0200

    Update solagraph configuration.

--- lua/cstm/lsp/server/solargraph.lua
+++ lua/cstm/lsp/server/solargraph.lua
@@ -1,7 +1,12 @@
 local nvim_lsp = require('lspconfig')
 
 local setup = function(on_attach)
-  nvim_lsp.solargraph.setup({ on_attach = on_attach })
+  nvim_lsp.solargraph.setup({
+      on_attach = on_attach,
+      flags = {
+        debounce_text_changes = 150,
+      }
+    })
 end
 
 return { setup = setup }
