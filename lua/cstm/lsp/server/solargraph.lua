d9ca1fa64d7eef1658a56c2311f7fde961907aa5
Aliou Diallo <code@aliou.me>
2021-07-23 20:48:14 +0200

    Remove formatting capabilities from solargraph.
    Close #54.

--- lua/cstm/lsp/server/solargraph.lua
+++ lua/cstm/lsp/server/solargraph.lua
@@ -1,12 +1,18 @@
 local nvim_lsp = require('lspconfig')
 
 local setup = function(on_attach)
+  local on_local_attach = function(client, bufnr)
+    -- Remove formatting capabilities to let efm handle it.
+    client.resolved_capabilities.document_formatting = false
+    on_attach(client, bufnr)
+  end
+
   nvim_lsp.solargraph.setup({
-      on_attach = on_attach,
-      flags = {
-        debounce_text_changes = 150,
-      }
-    })
+    on_attach = on_local_attach,
+    flags = {
+      debounce_text_changes = 150,
+    }
+  })
 end
 
 return { setup = setup }
