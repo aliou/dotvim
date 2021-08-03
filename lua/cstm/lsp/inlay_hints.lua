50ffd421eef528dec816e3e7c5fa0758f19be0ea
Aliou Diallo <code@aliou.me>
2021-08-03 05:16:05 +0200

    Only setup inlay hints for rust_analyzer.

--- lua/cstm/lsp/inlay_hints.lua
+++ lua/cstm/lsp/inlay_hints.lua
@@ -1,13 +1,9 @@
 local map = require('cstm.util').map
 local inlay_hints = require('cstm.buffer.inlay_hints')
 
--- Setup inlay hints in this file.
--- They seem to only be available with Rust and rust-analyzer, so explicitly
--- check that we're in a Rust buffer.
-local on_attach = function(_)
-  if vim.api.nvim_buf_get_option(0, 'filetype') ~= 'rust' then
-    return
-  end
+-- Setup inlay hints in this file, but only for rust_analyzer.
+local on_attach = function(client)
+  if client.name ~= "rust_analyzer" then return end
 
   -- Enable hints for the current buffer.
   inlay_hints.enable()
