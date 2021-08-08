dada57a69723f2f80058c5a7e7717849a97cb9a2
Aliou Diallo <code@aliou.me>
2021-08-08 20:23:27 +0200

    Clean search completion code.

--- lua/cstm/search.lua
+++ lua/cstm/search.lua
@@ -1,19 +1,7 @@
 local u = require('cstm.util')
 
--- TODO: There's a functional library for Lua that exists, might be interesting
--- to have it in dotfiles to avoid having to re-implement those when needed.
-local filter_args = function(argv)
-  local filtered_args = {}
-
-  for _, v in ipairs(argv) do
-    if v ~= "" then table.insert(filtered_args, v) end
-  end
-
-  return filtered_args
-end
-
+-- TODO: Filter out hidden directories by default?
 local complete_directories = function(lead)
-  -- TODO: Filter out hidden directories by default?
   local filter_directories = function(entry)
     local is_directory = vim.fn.isdirectory(entry) == 1
     local starts_with_lead = lead == "" or vim.startswith(entry, lead)
@@ -24,14 +12,13 @@ local complete_directories = function(lead)
   return vim.fn.readdir('.', filter_directories)
 end
 
+-- TODO: Add tags from current project using `taglist()`.
 local complete_term = function(lead)
   local lines = vim.fn.join(vim.fn.getline(1, '$'))
   -- Using vim.fn.split instead of vim.split because we can use actual regular
   -- expressions instead of Lua's patterns.
   local uniq_words = u.fn.compact(u.fn.uniq(vim.fn.split(lines, [[[[:blank:],.;:!?%#*+^@&/~|=<>\[\](){}]\+]])))
   local words = u.fn.sort(uniq_words)
-
-  -- if true then return {} end
 
   -- If there's no lead, don't bother filtering the words.
   if lead == "" then return words end
@@ -48,7 +35,7 @@ local complete_term = function(lead)
 end
 
 local complete = function(arg_lead, cmd_line, cursor_pos)
-  local argv = filter_args(vim.split(cmd_line, ' '))
+  local argv = u.fn.compact(vim.split(cmd_line, ' '))
   print(vim.inspect({ arg_lead = arg_lead, cursor_pos = cursor_pos, argv = argv, argc = #argv }))
 
   -- If we only have one argument (the command) and our position is after it,
