54969d8da0430750f81bd99c053680592f0b7bef
Aliou Diallo <code@aliou.me>
2021-09-11 10:37:40 +0200

    Fix lua path

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -1,9 +1,10 @@
--- This is used by apathy to setup the include path.
 local runtime_paths = vim.split(package.path, ';')
+
 table.insert(runtime_paths, 'lua/?.lua')
 table.insert(runtime_paths, 'lua/?/init.lua')
-table.insert(vim.g.lua_path, vim.fn.expand('~/.vim/lua/?.lua'))
-table.insert(vim.g.lua_path, vim.fn.getenv('VIMRUNTIME') .. '/lua')
+table.insert(runtime_paths, './?/init.lua')
+table.insert(runtime_paths, vim.fn.getenv('VIMRUNTIME') .. '/lua')
+table.insert(runtime_paths, vim.fn.expand('~/.vim/lua/?.lua'))
 
 -- TODO: Also include path to packages added by packer.nvim using runtime files.
 -- local runtime_files = vim.api.nvim_get_runtime_file('', true)
