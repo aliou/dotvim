6e834b9ffa5959f456d52657a81f360551856d5a
Aliou Diallo <code@aliou.me>
2021-12-03 22:03:10 +0100

    Toggle diagnostics in current buffer.

--- lua/ad/diagnostic.lua
+++ lua/ad/diagnostic.lua
@@ -1,5 +1,9 @@
 local map = require('cstm.util').map
 
+local gnmap = function(key, result)
+  map("n", key, result, { buffer = false })
+end
+
 local next = function()
   vim.diagnostic.goto_next({ wrap = true })
 end
@@ -8,17 +12,35 @@ local prev = function()
   vim.diagnostic.goto_prev({ wrap = true })
 end
 
+local setup = function()
+  vim.b.diagnostic_displayed = true
+end
+
+local toggle = function()
+  if vim.b.diagnostic_displayed then
+    vim.diagnostic.hide(nil, 0)
+    vim.b.diagnostic_displayed = false
+  else
+    vim.diagnostic.show(nil, 0)
+    vim.b.diagnostic_displayed = true
+  end
+end
+
 -- Navigate around errors or warnings.
-map("n", "[a", "<cmd>lua require('ad.diagnostic').prev()<cr>", { buffer = false })
-map("n", "]a", "<cmd>lua require('ad.diagnostic').next()<cr>", { buffer = false })
+gnmap("[a", "<cmd>lua require('ad.diagnostic').prev()<cr>")
+gnmap("]a", "<cmd>lua require('ad.diagnostic').next()<cr>")
+gnmap("<leader>at", "<cmd>lua require('ad.diagnostic').toggle()<cr>")
 
 -- Setup diagnostics default values.
--- Can be overriden in vimrc.local files.
+-- Can be overriden in vimrc.local files by clearing the autocmd augroup.
 vim.cmd [[ augroup ad.diagnostic ]]
 vim.cmd [[   autocmd! ]]
+vim.cmd [[   autocmd BufNew * :lua require('ad.diagnostic').setup() ]]
 vim.cmd [[ augroup END ]]
 
 return {
-  prev = prev,
   next = next,
+  prev = prev,
+  setup = setup,
+  toggle = toggle,
 }
