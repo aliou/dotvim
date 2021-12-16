10890225cff0075033c16010e796bd7a3c7946ed
Aliou Diallo <code@aliou.me>
2021-12-16 23:14:43 +0100

    Use vim.ui for ad.search.

--- lua/ad/search/init.lua
+++ lua/ad/search/init.lua
@@ -1,9 +1,4 @@
-local Input = require("nui.input")
-local event = require("nui.utils.autocmd").event
-
 local grep_string = require('telescope.builtin').grep_string
-
--------------------------------------------------
 
 -- TODO: State to allow history navigation and completion.
 -- TODO: Toggle regexp: on keymap toggle regex and update input title (like
@@ -18,43 +13,29 @@ local on_submit = function(term)
   grep_string({ search = term })
 end
 
-local on_close = function()
-  -- Get back to normal mode when closing the Input.
-  vim.cmd('stopinsert')
+local exec = function(term)
+  -- Fallback to word under cursor if no term is given.
+  -- TODO: Try to have this work differently depending on the language (thanks
+  -- to treesitter?).
+  if not term or #term == 0 then
+    term = vim.fn.expand("<cword>")
+  end
+
+  -- TODO: Update (vim|ad).ui.input to allow additional configuration of
+  -- nui.input. (in particular adding keymaps for history and other options, see
+  -- above).
+
+  local options = {
+    prompt = "Search: ",
+    default = term,
+  }
+  vim.ui.input(options, on_submit)
 end
 
--------------------------------------------------
-
-local input_options = {
-  border = {
-    style = "rounded",
-    text = { top = "[Search]", },
-  },
-  relative = "editor",
-  position = "50%",
-  size = { width = 30, height = 2 },
-}
-
-local start = function()
-  local input = Input(input_options, {
-    default_value = " ",
-    on_submit = on_submit,
-    on_close = on_close,
-  })
-
-  input:mount()
-
-  -- close on <esc> in normal mode
-  input:map("i", "<esc>", input.input_props.on_close, { noremap = true })
-
-  -- close when cursor leaves the buffer
-  input:on(event.BufLeave, input.input_props.on_close, { once = true })
-end
-
-vim.api.nvim_exec([[
-  nnoremap <silent> <leader>s <cmd>lua require('ad.search').start()<cr>
-]], true)
+vim.cmd [[
+  nnoremap <silent> <leader>s <cmd>lua require('ad.search').exec()<cr>
+]]
 
 return {
-  start = start
+  exec = exec
 }
