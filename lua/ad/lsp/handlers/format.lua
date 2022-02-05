a71b141d893b4107f0f40d27daf063954f70be3a
Aliou Diallo <code@aliou.me>
2022-02-05 10:40:49 +0100

    Fix format handler.

--- lua/ad/lsp/handlers/format.lua
+++ lua/ad/lsp/handlers/format.lua
@@ -1,10 +1,12 @@
-local apply_format = function(_, result, ctx)
+local apply_format = function(_, result, ctx, _)
   if not result or result == 0 then return end
+
+  local client = vim.lsp.get_client_by_id(ctx.client_id)
 
   -- To not have the formatting update be too jarring, save and restore the view
   -- of the current window around the application of the formatting.
   local view = vim.fn.winsaveview()
-  vim.lsp.util.apply_text_edits(result, ctx.bufnr)
+  vim.lsp.util.apply_text_edits(result, ctx.bufnr, client.offset_encoding)
   vim.fn.winrestview(view)
 
   -- Write changes witout triggering autocommands.
