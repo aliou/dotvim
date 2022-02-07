9c456e4b042e3281a9adace86725ba5d56f340b2
Aliou Diallo <code@aliou.me>
2022-02-07 14:49:35 +0100

    Fix diagnostic navigation mappings.

--- lua/ad/diagnostic.lua
+++ lua/ad/diagnostic.lua
@@ -43,8 +43,8 @@ local configure_theme = function(_)
 end
 
 -- Navigate around errors or warnings.
-vim.keymap.set('n', '[a', next, { desc = "[diagnostic] go to next" })
-vim.keymap.set('n', ']a', prev, { desc = "[diagnostic] go to prev" })
+vim.keymap.set('n', '[a', prev, { desc = "[diagnostic] go to prev" })
+vim.keymap.set('n', ']a', next, { desc = "[diagnostic] go to next" })
 vim.keymap.set('n', '<leader>at', toggle, { desc = "[diagnostic] toggle display of diagnostic" })
 
 -- Setup diagnostics default values.
