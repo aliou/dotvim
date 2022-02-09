ed635ceea0784dae03de8c1826f7dfefe23364b9
Aliou Diallo <code@aliou.me>
2022-02-09 06:09:20 +0100

    Remove obsolete map helper.

--- lua/cstm/util.lua
+++ lua/cstm/util.lua
@@ -1,20 +1,3 @@
-local DEFAULT_MAP_OPTIONS = { noremap = true, silent = true, buffer = true }
-
-local map = function(mode, key, result, options)
-  options = options or {}
-
-  local map_options = vim.tbl_extend('force', DEFAULT_MAP_OPTIONS, options)
-  local only_for_buffer = map_options.buffer
-
-  map_options.buffer = nil
-
-  if only_for_buffer then
-    vim.api.nvim_buf_set_keymap(0, mode, key, result, map_options)
-  else
-    vim.api.nvim_set_keymap(mode, key, result, map_options)
-  end
-end
-
 -- Extract into own file when this becomes too big.
 local fn
 fn = {
@@ -60,5 +43,4 @@ fn = {
 
 return {
   fn = fn,
-  map = map,
 }
