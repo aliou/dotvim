deaa42ad9c3ca0edb0f006a8ba02846a322d6f73
Aliou Diallo <code@aliou.me>
2022-02-16 16:49:09 +0100

    vim.ui.select: Automatically select sole item.

--- lua/ad/ui/select.lua
+++ lua/ad/ui/select.lua
@@ -1,22 +1,9 @@
 local native_select = vim.ui.select
 
-local default_select = function(items, opts, on_choice)
-  native_select(items, opts, on_choice)
-end
-
-local code_action_select = function(items, opts, on_choice)
+return function(items, opts, on_choice)
   if #items == 1 then
     return on_choice(items[1])
   end
 
-  default_select(items, opts, on_choice)
-end
-
--- TODO: Figure out if it's a bad idea to auto-select the first item.
-return function(items, opts, on_choice)
-  if opts.kind == "codeaction" then
-    return code_action_select(items, opts, on_choice)
-  end
-
-  return default_select(items, opts, on_choice)
+  return native_select(items, opts, on_choice)
 end
