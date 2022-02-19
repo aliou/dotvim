43019b09b4389690b498bd0c551587d5d6c3b473
Aliou Diallo <code@aliou.me>
2022-02-19 21:22:36 +0100

    notify: Allow prefixing and ignoring messages.

--- lua/ad/ui/notify.lua
+++ lua/ad/ui/notify.lua
@@ -1,6 +1,28 @@
-local prev_notify = vim.notify
+vim.log.level = vim.log.levels.INFO
+
+local prefix_message = function(prefix, message)
+  if not prefix then
+    return message
+  end
+
+  return string.format("[%s] %s", prefix, message)
+end
 
 -- TODO: Ignore some messages, e.g. the "no code action available"
-return function(message, level, options)
-  prev_notify(message, level, options)
+-- TODO: Prefix messages with log level.
+return function(msg, level, options)
+  -- TODO: Check minimum level (vim.notify_level)
+  if vim.log.level > level then
+    return
+  end
+
+  local message = prefix_message(options.prefix, msg)
+
+  if level == vim.log.levels.ERROR then
+    vim.api.nvim_err_writeln(message)
+  elseif level == vim.log.levels.WARN then
+    vim.api.nvim_echo({{message, 'WarningMsg'}}, true, {})
+  else
+    vim.api.nvim_echo({{message}}, true, {})
+  end
 end
