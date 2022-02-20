8d729861761067a781f4d881919ff8e74bb44800
Aliou Diallo <code@aliou.me>
2022-02-20 17:12:34 +0100

    lsp: Try to configure solagraph using lsp settings

--- lua/ad/lsp/server/solargraph.lua
+++ lua/ad/lsp/server/solargraph.lua
@@ -4,6 +4,9 @@ local publish_diagnostics_handler = function(_, _, _, _)
   vim.notify_once('Ignored diagnostic from solagraph server', vim.log.levels.INFO, { prefix = { "ad.lsp" } })
 end
 
+-- TODO: At some point, investigate if we can speed up the server by always have
+-- it running instead of starting it often (similar to what
+-- rubocop-deamon does).
 local setup = function(on_attach, _)
   local on_local_attach = function(client, bufnr)
     -- Remove formatting capabilities to let efm handle it.
@@ -19,6 +22,22 @@ local setup = function(on_attach, _)
     on_attach = on_local_attach,
     handlers = {
       ['textDocument/publishDiagnostics'] = publish_diagnostics_handler,
+    },
+    -- NOTE: Custom settings are configued here but seem to be completely
+    -- ignored by either nvim or by solargraph. To correctly customize the
+    -- differnt capabilities we want to use, we therefore use the
+    -- `on_local_attach` function and the custom handler.
+    settings = {
+      solargraph = {
+        -- Disable diagnostics and formatting as efm + rubocop-deamon-wrapper
+        -- handles those.
+        diagnostics = false,
+        formatting = false,
+        autoformat = false,
+
+        -- Disable goto_definition as bundle-ctags will handle it.
+        definitions = false,
+      }
     },
     flags = {
       debounce_text_changes = 150,
