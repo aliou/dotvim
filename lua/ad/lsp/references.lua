1c2e1d3b9734e9c67fac9626ce042ef2c001b906
Aliou Diallo <code@aliou.me>
2022-02-20 17:21:32 +0100

    lsp: Remove unused files.

--- lua/ad/lsp/references.lua
+++ lua/ad/lsp/references.lua
@@ -1,7 +1,11 @@
+local find_references = function()
+  vim.lsp.buf.references({ includeDeclaration = false })
+end
+
 local on_attach = function(client)
   if not client.resolved_capabilities.find_references then return end
 
-  vim.keymap.set('n', '<C-\\>', vim.lsp.buf.references, { desc = "[lsp] references", buffer = true })
+  vim.keymap.set('n', '<C-\\>', find_references, { desc = "[lsp] references", buffer = true })
 end
 
 return {
