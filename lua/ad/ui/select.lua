358201a6a468fd2b447ed2d24c1b618a5d78bf1e
Aliou Diallo <code@aliou.me>
2022-03-29 13:51:28 +0200

    vim.ui.select: Allow customization of handling based on kind

--- lua/ad/ui/select.lua
+++ lua/ad/ui/select.lua
@@ -1,9 +1,33 @@
 local native_select = vim.ui.select
 
-return function(items, opts, on_choice)
-  if #items == 1 then
+-- NOTE: It shouldn't really be possible to have the codeaction
+-- be automatically executed as we have a custom handler below,
+-- but for the sake of completeness, we still add it to the deny list.
+-- In the future, we might make sure that custom handlers are automatically
+-- added to the deny list.
+local kind_auto_execute_deny_list = { ["codeaction"] = true }
+
+local code_action_select = function(items, opts, on_choice)
+  return native_select(items, opts, on_choice)
+end
+
+local fallback_select = function (items, opts, on_choice)
+  if #items == 1 and not kind_auto_execute_deny_list[opts.kind] then
     return on_choice(items[1])
   end
 
   return native_select(items, opts, on_choice)
+end
+
+local custom_select_handlers = {
+  ["codeaction"] = code_action_select,
+}
+
+return function(items, opts, on_choice)
+  local custom_handler = custom_select_handlers[opts.kind]
+  if custom_handler then
+    return custom_handler(items, opts, on_choice)
+  end
+
+  fallback_select(items, opts, on_choice)
 end
