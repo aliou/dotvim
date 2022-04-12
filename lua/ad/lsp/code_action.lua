8767c400fb1242901c5e63b98350db52edc52133
Aliou Diallo <code@aliou.me>
2022-04-12 14:56:18 +0200

    ad.lsp: Add mapping for range code actions.

--- lua/ad/lsp/code_action.lua
+++ lua/ad/lsp/code_action.lua
@@ -2,6 +2,7 @@ local on_attach = function(client)
   if not client.resolved_capabilities.code_action then return end
 
   vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = "[lsp] code action", buffer = true })
+  vim.keymap.set('v', '<leader>ca', ':<c-u>lua vim.lsp.buf.range_code_action()<cr>', { desc = "[lsp] code action", buffer = true })
 end
 
 return {
