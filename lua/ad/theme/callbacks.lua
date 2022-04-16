fa37fc735cd2394124de09bd7bbfc9199de54b14
Aliou Diallo <code@aliou.me>
2022-04-17 00:01:37 +0200

    ad.theme: Rewrite autocmds in lua

--- lua/ad/theme/callbacks.lua
+++ lua/ad/theme/callbacks.lua
@@ -10,6 +10,18 @@ local execute_on_theme_change = function(theme)
   end
 end
 
+local autocmd_callback = function(options)
+  execute_on_theme_change(options.match)
+end
+
+-- Apply callbacks on colorscheme change.
+local augroup = vim.api.nvim_create_augroup('ad.theme', { clear = true })
+vim.api.nvim_create_autocmd('ColorScheme', {
+  group = augroup, pattern = '*', callback = autocmd_callback,
+  desc = '[theme] Execute all callbacks when chaning colorscheme'
+})
+
+
 return {
   execute_on_theme_change = execute_on_theme_change,
   on_theme_change = on_theme_change,
--- lua/ad/theme/init.lua
+++ lua/ad/theme/init.lua
@@ -1,11 +1,5 @@
 local watcher = require('ad.theme.watcher')
 
--- Apply callbacks on colorscheme change.
-vim.cmd [[augroup ad.theme]]
-vim.cmd [[  autocmd!]]
-vim.cmd [[  autocmd ColorScheme * :lua require('ad.theme.callbacks').execute_on_theme_change(vim.fn.expand('<amatch>'))]]
-vim.cmd [[augroup END]]
-
 -- Set default configuration. There's a good chance this is overriden on a per computer
 -- basis in ~/.vimrc.local.
 vim.g.colors = { light = 'github_light_default', dark = 'github_dark_default' }
