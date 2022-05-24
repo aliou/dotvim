0ab1376dfd59f19695819a921b465c4770c37b04
Aliou Diallo <code@aliou.me>
2022-05-24 18:20:36 +0200

    ad.lsp: setup eslint formatter, diagnostic and code_actions

--- lua/ad/lsp/server/null_ls.lua
+++ lua/ad/lsp/server/null_ls.lua
@@ -1,4 +1,5 @@
 local null_ls = require('null-ls')
+local command_resolver = require("null-ls.helpers.command_resolver")
 
 -- Register custom code actions.
 require('ad.lsp.server.null_ls.go')
@@ -8,12 +9,25 @@ require('ad.lsp.server.null_ls.shared')
 local jq = require('ad.lsp.server.null_ls.jq')
 local rubocop = require('ad.lsp.server.null_ls.rubocop')
 
+local eslint_overrides = {
+  dynamic_command = function(params)
+    return command_resolver.from_yarn_pnp(params)
+      or command_resolver.from_node_modules(params)
+      or vim.fn.executable(params.command) == 1 and params.command
+  end,
+}
+
 local sources = {
+  null_ls.builtins.formatting.eslint.with(eslint_overrides),
   null_ls.builtins.formatting.trim_whitespace.with({
     -- Remove filetypes who already remove whitespace when formatting.
     disabled_filetypes = { "go" },
   }),
+
+  null_ls.builtins.diagnostics.eslint.with(eslint_overrides),
   null_ls.builtins.diagnostics.shellcheck,
+
+  null_ls.builtins.code_actions.eslint.with(eslint_overrides),
   null_ls.builtins.code_actions.shellcheck,
 
   jq.formatter,
