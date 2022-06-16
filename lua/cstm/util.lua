e21a420bc7cdc08e06942049882c95be631eec61
Aliou Diallo <code@aliou.me>
2022-06-16 17:59:21 +0200

    util: remove unused function

--- lua/cstm/util.lua
+++ lua/cstm/util.lua
@@ -21,17 +21,6 @@ fn = {
     return vim.fn.sort(vim.fn.copy(t))
   end,
 
-  -- TODO: Allow keeping the keys?
-  filter = function(t, func)
-    local filtered_args = {}
-
-    for _, v in ipairs(t) do
-      if func(v) then table.insert(filtered_args, v) end
-    end
-
-    return filtered_args
-  end,
-
   compact = function(t)
     local filter = function(v)
       return v ~= "" and (not (not v))
