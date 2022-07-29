8fc399a3ca50294ba54b7a4fc5acd13f08aa9371
Aliou Diallo <code@aliou.me>
2022-07-29 16:48:22 +0200

    diagnostic: add success highlight

--- lua/ad/diagnostic/theme.lua
+++ lua/ad/diagnostic/theme.lua
@@ -6,6 +6,7 @@ local configure_theme = function(_)
   vim.highlight.create('DiagnosticWarn', { ctermfg = 173, gui = 'bold', guifg = '#de935f' })
   vim.highlight.create('DiagnosticInfo', { cterm = 'bold', ctermfg = 60, gui = 'bold', guifg = '#5F5F87' })
   vim.highlight.create('DiagnosticHint', { cterm = 'bold', ctermfg = 173, gui = 'bold', guifg = '#c7915b' })
+  vim.highlight.create('DiagnosticSuccess', { guifg = "#5F875F", ctermfg = 65 })
 end
 
 -- Re-apply highlights on theme changes.
