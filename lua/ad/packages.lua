0b9ecb431fec29a562b0bcaf905ea39d690de43a
Aliou Diallo <code@aliou.me>
2022-07-29 22:02:07 +0200

    Store plugin revisions in snapshot.

--- lua/ad/packages.lua
+++ lua/ad/packages.lua
@@ -1,77 +1,77 @@
 vim.cmd [[ packadd packer.nvim ]]
 
-local spec = function()
-  use { 'wbthomason/packer.nvim', opt = true }
+local packages = {
+  { 'wbthomason/packer.nvim', opt = true },
 
   -- Display
-  use { 'itchyny/lightline.vim' }
-  use { 'mhinz/vim-startify' }
-  use { 'junegunn/goyo.vim', opt = true }
-  use { 'MunifTanjim/nui.nvim' }
-  use { 'projekt0n/github-nvim-theme' }
-  use { 'p00f/alabaster_dark.nvim', requires = { 'rktjmp/lush.nvim' } }
-  use { 'b0o/incline.nvim' }
+  { 'itchyny/lightline.vim' },
+  { 'mhinz/vim-startify' },
+  { 'junegunn/goyo.vim', opt = true },
+  { 'MunifTanjim/nui.nvim' },
+  { 'projekt0n/github-nvim-theme' },
+  { 'p00f/alabaster_dark.nvim', requires = { 'rktjmp/lush.nvim' } },
+  { 'b0o/incline.nvim' },
 
   -- Shell
-  use { 'tpope/vim-dispatch' }
-  use { 'tpope/vim-eunuch' }
-  use { 'tpope/vim-fugitive' }
-  use { 'tpope/vim-rhubarb' }
+  { 'tpope/vim-dispatch' },
+  { 'tpope/vim-eunuch' },
+  { 'tpope/vim-fugitive' },
+  { 'tpope/vim-rhubarb' },
 
   -- Editing
-  use { 'AndrewRadev/splitjoin.vim' }
-  use { 'andymass/vim-matchup' }
-  use { 'tpope/vim-commentary' }
-  use { 'tpope/vim-sleuth' }
-  use { 'tpope/vim-surround' }
-  use { 'mg979/vim-visual-multi' }
+  { 'AndrewRadev/splitjoin.vim' },
+  { 'andymass/vim-matchup' },
+  { 'tpope/vim-commentary' },
+  { 'tpope/vim-sleuth' },
+  { 'tpope/vim-surround' },
+  { 'mg979/vim-visual-multi' },
 
   -- Utils.
-  use {
+  {
     'lewis6991/gitsigns.nvim',
     requires = { 'nvim-lua/plenary.nvim' }
-  }
-  use { 'ludovicchabant/vim-gutentags' }
-  use { 'tpope/vim-repeat' }
-  use { 'tpope/vim-unimpaired' }
-  use { 'tpope/vim-vinegar' }
-  use { 'junegunn/fzf.vim' }
+  },
+  { 'ludovicchabant/vim-gutentags' },
+  { 'tpope/vim-repeat' },
+  { 'tpope/vim-unimpaired' },
+  { 'tpope/vim-vinegar' },
+  { 'junegunn/fzf.vim' },
 
   -- Projects
-  use { 'tpope/vim-apathy' }
-  use { 'tpope/vim-bundler' }
-  use { 'tpope/vim-obsession', cmd = { 'Obsession' } }
-  use { 'tpope/vim-projectionist' }
-  use { 'tpope/vim-rails' }
-  use { 'tpope/vim-rake' }
-  use { 'tpope/vim-scriptease' }
+  { 'tpope/vim-apathy' },
+  { 'tpope/vim-bundler' },
+  { 'tpope/vim-obsession', cmd = { 'Obsession' } },
+  { 'tpope/vim-projectionist' },
+  { 'tpope/vim-rails' },
+  { 'tpope/vim-rake' },
+  { 'tpope/vim-scriptease' },
 
   -- Languages
-  use { 'ap/vim-css-color' }
-  use { 'chr4/nginx.vim' }
-  use { 'elixir-editors/vim-elixir' }
-  use { 'hashivim/vim-terraform' }
-  use { 'keith/swift.vim' }
-  use { 'lifepillar/pgsql.vim' }
-  use { 'tbastos/vim-lua' }
-  use { 'vim-ruby/vim-ruby' }
-  use { 'vito-c/jq.vim' }
+  { 'ap/vim-css-color' },
+  { 'chr4/nginx.vim' },
+  { 'elixir-editors/vim-elixir' },
+  { 'hashivim/vim-terraform' },
+  { 'keith/swift.vim' },
+  { 'lifepillar/pgsql.vim' },
+  { 'tbastos/vim-lua' },
+  { 'vim-ruby/vim-ruby' },
+  { 'vito-c/jq.vim' },
 
   -- Text object.
-  use { 'PeterRincker/vim-argumentative' }
+  { 'PeterRincker/vim-argumentative' },
 
   -- Tests / Lint / LSP
-  use { 'janko-m/vim-test' }
-  use { 'neovim/nvim-lspconfig' }
-  use { 'tjdevries/lsp_extensions.nvim' }
-  use { 'jose-elias-alvarez/null-ls.nvim' }
+  { 'janko-m/vim-test' },
+  { 'neovim/nvim-lspconfig' },
+  { 'tjdevries/lsp_extensions.nvim' },
+  { 'jose-elias-alvarez/null-ls.nvim' },
 
   -- Treesitter configuration and abstraction.
-  use { 'nvim-treesitter/nvim-treesitter' }
-  use { 'RRethy/nvim-treesitter-endwise' }
+  { 'nvim-treesitter/nvim-treesitter' },
+  { 'RRethy/nvim-treesitter-endwise' },
 
   -- Completion
-  use {
+  {
     'hrsh7th/nvim-cmp',
     requires = {
       'hrsh7th/cmp-nvim-lsp',
@@ -80,28 +80,42 @@ local spec = function()
       'hrsh7th/cmp-nvim-lua',
       'L3MON4D3/LuaSnip',
     }
-  }
+  },
 
   -- Local plugins.
 
   -- This requires fzf to be installed with go:
   -- `go get github.com/junegunn/fzf -u`.
-  use '~/code/src/github.com/junegunn/fzf'
+  '~/code/src/github.com/junegunn/fzf',
 
-  use '~/code/src/github.com/aliou/bats.vim'
-  use '~/code/src/github.com/aliou/dope.vim'
-  use '~/code/src/github.com/aliou/hybrid.vim'
-  use { '~/code/src/github.com/aliou/mando', rtp = 'vim' }
-  use '~/code/src/github.com/aliou/markoff.vim'
-  use '~/code/src/github.com/aliou/mix.vim'
-  use '~/code/src/github.com/aliou/moriarty.vim'
-  use '~/code/src/github.com/aliou/phx.vim'
-  use '~/code/src/github.com/aliou/review.vim'
-  use '~/code/src/github.com/aliou/ri.vim'
-  use '~/code/src/github.com/aliou/rspec-extra.vim'
-  use '~/code/src/github.com/aliou/sql-heredoc.vim'
-  use '~/code/src/github.com/aliou/untitled.vim'
-  use '~/code/src/github.com/aliou/wren.vim'
-end
+  '~/code/src/github.com/aliou/bats.vim',
+  '~/code/src/github.com/aliou/dope.vim',
+  '~/code/src/github.com/aliou/hybrid.vim',
+  { '~/code/src/github.com/aliou/mando', rtp = 'vim' },
+  '~/code/src/github.com/aliou/markoff.vim',
+  '~/code/src/github.com/aliou/mix.vim',
+  '~/code/src/github.com/aliou/moriarty.vim',
+  '~/code/src/github.com/aliou/phx.vim',
+  '~/code/src/github.com/aliou/review.vim',
+  '~/code/src/github.com/aliou/ri.vim',
+  '~/code/src/github.com/aliou/rspec-extra.vim',
+  '~/code/src/github.com/aliou/sql-heredoc.vim',
+  '~/code/src/github.com/aliou/untitled.vim',
+  '~/code/src/github.com/aliou/wren.vim',
+}
 
-require('packer').startup(spec)
+local config = { snapshot_path = vim.fn.expand('~/.vim') }
+require('packer').startup({ packages, config = config, rocks = {} })
+
+-- TODO: Checksum and auto update of packages.
+local snapshot_name = '.packages-snapshot'
+
+vim.api.nvim_del_user_command('PackerSnapshot')
+vim.api.nvim_create_user_command('PackerSnapshot', function()
+  require('packer').snapshot(snapshot_name)
+end, { desc = 'serialize installed plugins' })
+
+vim.api.nvim_del_user_command('PackerSnapshotRollback')
+vim.api.nvim_create_user_command('PackerSnapshotRollback', function()
+  require('packer').rollback(snapshot_name)
+end, { desc = 'rollbacks plugins to version in snapshot' })
