a03d8861687d0edf69ceb4b0b5ae5de5c061f3de
Aliou Diallo <code@aliou.me>
2022-09-22 15:14:32 +0200

    lsp: add print var code action

--- lua/ad/lsp/server/null_ls/shared.lua
+++ lua/ad/lsp/server/null_ls/shared.lua
@@ -42,5 +42,33 @@ local binding_pry = {
   }
 }
 
+local PRINT_STR = 'console.log("DEBUG", { %s });'
+local print_var = {
+  method = null_ls.methods.CODE_ACTION,
+  filetypes = { "javascript", "typescript" },
+  generator = {
+    fn = function(context)
+      local current_line = context.content[context.row]
+      local current_word = vim.fn.expand("<cword>")
+
+      local print_statement = string.format(PRINT_STR, current_word)
+
+      local indent = vim.fn.indent(context.row)
+      local lines = { current_line, u.indent_str(print_statement, indent) }
+
+      return {
+        {
+          title = "Insert print statement for variable",
+          action = function()
+            vim.api.nvim_buf_set_lines(
+              context.bufnr, context.row - 1, context.row, false, lines
+            )
+          end
+        }
+      }
+    end
+  }
+}
 
 null_ls.register(binding_pry)
+null_ls.register(print_var)
