fb67b2581c740a0711b6b2e8b44380dbbb42efcd
Aliou Diallo <code@aliou.me>
2022-12-14 09:42:51 +0100

    init: Correctly update runtimepath and packpath.

--- init.lua
+++ init.lua
@@ -1,8 +1,11 @@
 -- Setup runtimepath and packpath to include ~/.vim directory.
 -- Set the vim directory as the first directory to search into and the after
 -- directory as the last to search into.
-vim.opt.runtimepath = vim.opt.runtimepath:prepend({'~/.vim'}):append({'~/.vim/after'})
-vim.opt.packpath = vim.opt.packpath:prepend({'~/.vim'}):append({'~/.vim/after'})
+vim.opt.runtimepath:prepend({'~/.vim'})
+vim.opt.runtimepath:append({'~/.vim/after'})
+
+vim.opt.packpath:prepend({'~/.vim'})
+vim.opt.packpath:append({'~/.vim/after'})
 
 -- Setting up early so mappings with <leader> use the this value.
 vim.g.mapleader = ','
