1194a2558d87b7eafd5c3286ac61f998158a7764
Aliou Diallo <code@aliou.me>
2022-12-14 10:19:36 +0100

    lsp: Let null-ls handle formatting for ts files.

--- lua/ad/lsp/server/tsserver.lua
+++ lua/ad/lsp/server/tsserver.lua
@@ -2,7 +2,13 @@ local nvim_lsp = require('lspconfig')
 
 
 local setup = function(on_attach)
-  nvim_lsp.tsserver.setup({ on_attach = on_attach })
+  local on_local_attach = function(client, bufnr)
+    -- Remove formatting capabilities to let eslint handle it.
+    client.server_capabilities.documentFormattingProvider = nil
+    on_attach(client, bufnr)
+  end
+
+  nvim_lsp.tsserver.setup({ on_attach = on_local_attach })
 end
 
 return { setup = setup }
