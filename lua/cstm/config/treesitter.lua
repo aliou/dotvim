88ac2323850ed9092c16ea8bf569cb9880385065
Aliou Diallo <code@aliou.me>
2023-04-24 14:22:06 +0200

    treesitter: Add playground other languages

--- lua/cstm/config/treesitter.lua
+++ lua/cstm/config/treesitter.lua
@@ -15,6 +15,8 @@ require('nvim-treesitter.configs').setup({
     "lua",
     "make",
     "markdown",
+    "prisma",
+    "query",
     "ruby",
     "rust",
     "scss",
@@ -31,10 +33,27 @@ require('nvim-treesitter.configs').setup({
     enable = true,
   },
   indent = {
-    enable = true,
-    disable = { 'ruby' }
+    enable = false,
   },
   endwise = {
     enable = true,
   },
+  playground = {
+    enable = true,
+    disable = {},
+    updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+    persist_queries = false, -- Whether the query persists across vim sessions
+    keybindings = {
+      toggle_query_editor = 'o',
+      toggle_hl_groups = 'i',
+      toggle_injected_languages = 't',
+      toggle_anonymous_nodes = 'a',
+      toggle_language_display = 'I',
+      focus_language = 'f',
+      unfocus_language = 'F',
+      update = 'R',
+      goto_node = '<cr>',
+      show_help = '?',
+    },
+  }
 })
