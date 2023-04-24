417b7778c4a9340bf57d14bfb424570b7aeea3d1
Aliou Diallo <code@aliou.me>
2023-04-24 11:14:13 +0200

    ui: Remove incline

--- lua/ad/ui/status/init.lua
+++
@@ -1,6 +0,0 @@
--- Display a single status line.
-vim.o.laststatus = 3
-
--- Configure incline.
-require('ad.ui.status.theme')
-require('ad.ui.status.incline')
--- lua/ad/ui/status/theme.lua
+++
@@ -1,8 +0,0 @@
-local theme_callbacks = require('ad.theme.callbacks')
-
-local configure_theme = function(_)
-  vim.api.nvim_set_hl(0, 'InclineNormal', { fg = '#b4b4b4', bg = '#373737'})
-  vim.api.nvim_set_hl(0, 'InclineNormalNC', { fg = '#b4b4b4', bg = '#373737'})
-end
-
-theme_callbacks.on_theme_change('ad.ui.status', configure_theme)
