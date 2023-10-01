649d031ac4e6a7cb53d3eaedb4e2545290485826
Aliou Diallo <code@aliou.me>
2023-10-01 19:25:28 +0200

    lsp: remove unused mapping

--- lua/ad/lsp/code_action.lua
+++ lua/ad/lsp/code_action.lua
@@ -2,7 +2,6 @@ local on_attach = function(client)
   if not client.server_capabilities.codeActionProvider then return end
 
   vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = "[lsp] code action", buffer = true })
-  vim.keymap.set('v', '<leader>ca', ':<c-u>lua vim.lsp.buf.range_code_action()<cr>', { desc = "[lsp] code action", buffer = true })
 end
 
 return {
